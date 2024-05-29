c69a6af4dd5664fa03e65982b388438172c35f89
A-nshuman <abtbbruv@gmail.com>
2024-05-29 12:27:58 +0300

    title remove

--- next-js/app/layout.tsx
+++ next-js/app/layout.tsx
@@ -2,10 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Chakra_Petch } from "next/font/google";
 import "../styles/globals.css";
-import { Head } from "next/document";
 
 const inter = Inter({ subsets: ["latin"] });
-const chakra = Chakra_Petch({ subsets: ["latin"], weight: ['300'] });
+const chakra =  Chakra_Petch({ subsets: ["latin"], weight: ['300'] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,9 +18,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <title>Credit Card Validator</title>
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
